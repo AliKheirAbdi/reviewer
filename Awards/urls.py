f2d75b70b68e22b0b42807e5dd05d93702a48d57
AliKheir <akheirali@gmail.com>
2019-11-23 17:43:48 +0300

    created register,login,profile routes to urls.py

---
+++ Awards/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, re_path
+from . import views
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('register/', views.registration, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('profile/', views.profile, name='profile'),
+
+
+
+
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
