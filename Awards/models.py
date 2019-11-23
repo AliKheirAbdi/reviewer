4adec559f0814a99495f86f7a894ff0dcb5eda81
AliKheir <akheirali@gmail.com>
2019-11-23 17:54:49 +0300

    created Profile models in models.py

--- Awards/models.py
+++ Awards/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch  import receiver
+from django.http import Http404
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.png', upload_to='profile_pics/')
+    bio = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    class Meta:
+        db_table = 'profile'
+
+    @receiver(post_save, sender=User)
+    def update_create_profile(sender,instance,created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
+
+
+    def save_profile(self):
+        self.save()
