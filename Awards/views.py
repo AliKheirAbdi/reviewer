0ee38919c7e9c22ffcf8213d6db27bdb48eee43a
AliKheir <akheirali@gmail.com>
2019-11-23 19:52:12 +0300

    created user profile view pointing to profile.html

--- Awards/views.py
+++ Awards/views.py
@@ -25,3 +25,28 @@ def registration(request):
         'form':form,
     }
     return render(request, 'users/register.html', context)
+
+
+@login_required
+def profile(request):
+    projects = Projects.objects.all()
+    posts = Profile.objects.all()
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Your account has been successfully updated')
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+    'user_form':user_form,
+    'profile_form':profile_form,
+    'posts':posts,
+    'projects':projects,
+    }
+    return render(request, 'profile.html', context)
