054fce74c889f66662b1ae5ee5892dbd754566f6
AliKheir <akheirali@gmail.com>
2019-11-23 19:48:51 +0300

    created homepage view pointing to home.html

--- Awards/views.py
+++ Awards/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
+
+def home(request):
+    projects = Projects.objects.all()
+    context = {
+        "projects":projects,
+    }
+    return render(request, 'home.html', context)
