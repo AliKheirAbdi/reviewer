a2d77855f6e8f3e843fe4bdf788d8e54c5570924
AliKheir <akheirali@gmail.com>
2019-11-25 00:39:21 +0300

    created Post test

--- Awards/tests.py
+++ Awards/tests.py
@@ -15,3 +15,35 @@ class TestProfile(TestCase):
 
     def test_delete_user(self):
         self.user.delete()
+
+
+class PostTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id=1, username='ali')
+        self.post = Post.objects.create(id=1, title='test post', photo='https://ucarecdn.com/ce19abdd-d59f-42da-8115-534bfa544b5c/-/preview/1162x693/-/setfill/ffffff/-/format/jpeg/-/progressive/yes/', description='desc',
+                                        user=self.user, url='http://ur.coml')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, Post))
+
+    def test_save_post(self):
+        self.post.save_post()
+        post = Post.objects.all()
+        self.assertTrue(len(post) > 0)
+
+    def test_get_posts(self):
+        self.post.save()
+        posts = Post.all_posts()
+        self.assertTrue(len(posts) > 0)
+
+    def test_search_post(self):
+        self.post.save()
+        post = Post.search_project('test')
+        self.assertTrue(len(post) > 0)
+
+    def test_delete_post(self):
+        self.post.delete_post()
+        post = Post.search_project('test')
+        self.assertTrue(len(post) < 1)
+
+
