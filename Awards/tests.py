548dc8309879092872dfeb50d463ee47f7010515
AliKheir <akheirali@gmail.com>
2019-11-25 00:31:12 +0300

    created Profile tests

--- Awards/tests.py
+++ Awards/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import *
 
 # Create your tests here.
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(id=1, username='ali', password='quest99')
+        self.user.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user, User))
+
+    def test_save_user(self):
+        self.user.save()
+
+    def test_delete_user(self):
+        self.user.delete()
