2d002e836dd258a949c1b217dd64646006e9fbae
AliKheir <akheirali@gmail.com>
2019-11-25 00:39:56 +0300

    created rating test

--- Awards/tests.py
+++ Awards/tests.py
@@ -47,3 +47,22 @@ class PostTest(TestCase):
         self.assertTrue(len(post) < 1)
 
 
+class RatingTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id=1, username='ali')
+        self.post = Post.objects.create(id=1, title='test post', photo='https://ucarecdn.com/ce19abdd-d59f-42da-8115-534bfa544b5c/-/preview/1162x693/-/setfill/ffffff/-/format/jpeg/-/progressive/yes/', description='desc',
+                                        user=self.user, url='http://ur.coml')
+        self.rating = Rating.objects.create(id=1, design=6, usability=7, content=9, user=self.user, post=self.post)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.rating, Rating))
+
+    def test_save_rating(self):
+        self.rating.save_rating()
+        rating = Rating.objects.all()
+        self.assertTrue(len(rating) > 0)
+
+    def test_get_post_rating(self, id):
+        self.rating.save()
+        rating = Rating.get_ratings(post_id=id)
+        self.assertTrue(len(rating) == 1)
