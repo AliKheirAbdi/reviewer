22d575f5ad6886572c4962113ad4603046c585b5
AliKheir <akheirali@gmail.com>
2019-11-24 14:42:40 +0300

    created serializers.py file in awards

---
+++ Awards/serializer.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import *
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Projects
+        fields = ['title', 'description', 'image', 'author', 'created_date', 'author_profile', 'link']
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['user', 'image', 'bio']
